07c0df4e31cfb089d69e2422f1f57aff649c9ac1
mss39 <mss39@aluno.ifal.edu.br>
2023-08-03 10:16:17 -0300

    Create Models and migrations Prestador and Servico

---
+++ database/migrations/2023_08_03_131059_create_servicos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('descricao');
+            $table->decimal('valor');
+            $table->unsignedBigInteger('id_prestador');
+            $table->timestamps();
+
+            $table->foreign('id_prestador')->references('id')->on('prestadores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicos');
+    }
+};
