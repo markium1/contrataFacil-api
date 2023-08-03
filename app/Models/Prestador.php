fd557893dc7e3fe0050ea9e1caa1357483bcd89e
mss39 <mss39@aluno.ifal.edu.br>
2023-08-03 11:35:27 -0300

    modified fillable add values

--- app/Models/Prestador.php
+++ app/Models/Prestador.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Prestador extends Model
 {
     use HasFactory;
+    protected $table = 'prestadores';
+    protected $fillable = ['nome', 'telefone', 'email', 'foto'];
 }
